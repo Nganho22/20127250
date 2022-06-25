4f097f66a6ccaa4bb4d5ac5228687aada6af1dba
nganho <nganho2251@gmail.com>
2022-06-25 12:28:20 +0700

    post get

--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
-app.use(express.static(path.join(__dirname, 'public')))
+//app.use(express.static(path.join(__dirname, 'public')))
+
+//app.use(express.urlencoded(
+  //  {
+    //    extended: true
+    //}
+//))
+//app.use(express.json)
 app.use(morgan('combined'))
 
 app.engine('hbs', engine(
@@ -29,8 +36,19 @@ app.get('/news', (req, res) => {
     //res.send('123')
 })
 
+app.get('/search', (req, res) => {
+    //   console.log(req.query)
+    res.render('search')
+})
+
+app.post('/search', (req, res) => {
+    console.log(req.body)
+    res.render('search')
+})
+
+
 //app.set('views', path.join(__dirname, 'resources/views'))
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-});
+})
